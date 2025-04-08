350458027966e1d672a213b40eda2163dce908aa
Rafael Petrov <djfanta@abv.bg>
2025-04-09 02:35:47 +0300

    Redirect to products page

--- controllers/productController.js
+++ controllers/productController.js
@@ -15,7 +15,7 @@ router.get('/create', (req, res) => {
 
 router.post('/create', (req, res) => {
     console.log(req.body);
-    res.send('created');
+    res.redirect('/products/create');
 });
 
 
