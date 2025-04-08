9e978dccebbee38a5b74245edbc58a5d0d9f956c
Rafael Petrov <djfanta@abv.bg>
2025-04-08 15:19:50 +0300

    Initial architecture

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const config = require('./config/config.js');
+
+const app = express();
+
+
+app.listen(config.PORT, () => {
+    console.log(`Server is running on port ${config.PORT}`);
+});  
