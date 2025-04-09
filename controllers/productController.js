3191b07b80b64d6ee179547958b6bf3e51699a16
Rafael Petrov <djfanta@abv.bg>
2025-04-09 15:55:50 +0300

    Add product layer

--- controllers/productController.js
+++ controllers/productController.js
@@ -17,13 +17,6 @@ router.get('/create', (req, res) => {
 
 
 router.post('/create', validateProduct, (req, res) => {
-    // productService.create(req.body, (err) => {
-    //     if (err) {
-    //         console.log(err);
-    //         return res.status(500).end();
-    //     }
-    //     res.redirect('/products/');
-    // });
     productService.create(req.body)
         .then(() => res.redirect('/products/'))
         .catch(() => res.status(500).end())
