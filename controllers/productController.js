f03defcfe578da93d876b2af65adb70dfaecb761
Rafael Petrov <djfanta@abv.bg>
2025-04-09 12:37:08 +0300

    Show all cubes on homepage

--- controllers/productController.js
+++ controllers/productController.js
@@ -4,7 +4,9 @@ const router = Router();
 
 
 router.get('/', (req, res) => {
-    res.render('home', {title: 'Browse'});
+    let products = productService.getAll();
+
+    res.render('home', {title: 'Browse', products });
 });
 
 
